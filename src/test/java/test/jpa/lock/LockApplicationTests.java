ccb783442fd449e4141dd820597bb93e949541dd
lass <lass9436@gmail.com>
2024-06-01 04:28:20 +0900

    Initial commit

---
+++ src/test/java/test/jpa/lock/LockApplicationTests.java
@@ -0,0 +1,13 @@
+package test.jpa.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
